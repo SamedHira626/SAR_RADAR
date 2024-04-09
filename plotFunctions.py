3baaa844f15bc16bb7082502b9ef0a81ca37c4c4
Samed Hıra <samedhira626@gmail.com>
2024-04-09 19:48:00 +0300

    plotting func is added

---
+++ plotFunctions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+
+def plot_images(images, headers, num):
+
+    if num <= 6:
+        rows = 2
+        cols = 3
+    else:
+        rows = 3
+        cols = 3
+
+    fig, axes = plt.subplots(rows, cols, figsize=(10, 10))
+    axes = axes.flatten()
+
+    for i in range(num):
+        axes[i].imshow(images[i], cmap='gray')
+        axes[i].set_title(headers[i])
+        axes[i].axis('off')
+
+    plt.tight_layout()
+    plt.show()
